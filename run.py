0c6c0944c3accb215c53eb98de0d6bebb13c3c42
Frankie141 <franceswatts17@gmail.com>
2022-02-23 23:01:17 +0000

    Created function to choose random word

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 # easy random work generator
 import random
+from words import words
 
 words = ['Python', 'JavaScript', 'Computer', 'player',
          'library', 'Peacock', 'Rainbow', 'Rock', 'Paper']
@@ -12,3 +13,10 @@ print("""
         letters to figure out the word. You will have 10 guesses to pick your 
         letters. Good luck name\n
     """)
+
+def get_random_word(words):
+    word = random_word.choice(words) # Chooses random word from the list
+    while '-' in word or ' ' word: 
+    word = random_word.choice(words)
+
+    return word    
