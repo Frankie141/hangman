14a3f73b3e80100f2ae145670bfdec5383e68bb1
Frankie141 <franceswatts17@gmail.com>
2022-02-23 22:51:44 +0000

    Initial Commit

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# easy random work generator
+import random
+
+words = ['Python', 'JavaScript', 'Computer', 'player',
+         'library', 'Peacock', 'Rainbow', 'Rock', 'Paper']
+
+# Welcome message and name input
+name = input("Please enter your name: ")
+print(""" 
+        Welcome name to Hangman. You will be playing against the computer.
+        The computer will randomly choose a word and you will need to choose 
+        letters to figure out the word. You will have 10 guesses to pick your 
+        letters. Good luck name\n
+    """)
